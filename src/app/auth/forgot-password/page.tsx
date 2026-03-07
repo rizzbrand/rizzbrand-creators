d5a1ddbc727c54f82c23896a4fa01602e55a01d2
rizzbrand <rizzbrandit@gmail.com>
2026-03-07 09:41:44 +0000

    update1.6

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-import { ForgotPasswordForm } from "@/components/auth/forgot-password-form";
+import ForgotPasswordForm from "@/components/auth/forgot-password-form";
 import { Suspense } from "react";
 
 export default function ForgotPasswordPage() {
--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,4 +1,4 @@
-import { ResetPasswordForm } from "@/components/auth/reset-password-form";
+import ResetPasswordForm from "@/components/auth/reset-password-form";
 import { Suspense } from "react";
 
 export default function ResetPasswordPage() {
